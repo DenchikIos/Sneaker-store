71ec97550b28a1e031eae3aaa59681b8d5d20763
Denis IOS Developer <denis.poddubnyy.111@gmail.com>
2024-02-21 17:18:07 +0300

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by User on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
